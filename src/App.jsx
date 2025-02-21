539d5553034f01e71e55b90a9ee820fa9fb7da74
nimshi <nimshihunukumbura12@gmail.com>
2025-02-21 19:09:31 +0530

    setup project and finished hero section

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Hero from './components/Hero'
+import ContentSection from './components/ContentSection'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Hero />
+      <ContentSection />
+    </div>
+  )
+}
+
+export default App
